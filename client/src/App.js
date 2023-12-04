84bc577534db7ffdbdaa6dd82c5827a4b978da5c
Oussama-hamdi <86133676+Oussama-hamdi@users.noreply.github.com>
2023-12-04 16:47:09 +0100

    Created home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import "./styles/Main.css";
 
 function App() {
-  return <h1 className="App">Hello World!!!</h1>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <h1>Hello there?</h1>
+    </>
+  );
 }
 
 export default App;
